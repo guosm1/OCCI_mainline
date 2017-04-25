1b0ccdf107b5aa4e6dd75eef827a5ab0bb6424e5
zhaoyim <yyyiming@163.com>
2017-04-25 17:27:59 +0800

    change level to drop down list

--- ockb/app/conf/config.js
+++ ockb/app/conf/config.js
@@ -2,6 +2,8 @@
 var configApp = angular.module('ockbConfigApp', []);
 
 configApp.constant('CONFIG', {
-                        "internalUse": false
+                        "internalUse": false,
+                        // the levels for the problem level in the create and edit modal
+                        "levels": ["INFO", "WARN", "ERROR", "FATAL"]
                     }
                   );
