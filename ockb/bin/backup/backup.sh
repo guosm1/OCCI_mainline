76a21a60ccad8012a99cae297e6e85132d842085
zhaoyim <yyyiming@163.com>
2017-05-22 11:16:27 +0800

    update backup scripts

--- ockb/bin/backup/backup.sh
+++ ockb/bin/backup/backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-source ../elasticdump-env
+source /opt/ockb/bin/elasticdump-env
 
 filename=`date +%Y%m%d`
 # dump the ES data
-elasticdump --input=http://<ex server>:<es port>/occikb --output=./$filename.json --type=data
+elasticdump --input=http://<ex server>:<es port>/occikb --output=/opt/ockb/bin/backup/$filename.json --type=data
 
 #remove the before 7 days data
 find /opt/ockb/bin/backup/ -name '*.json' -type f -mtime +7 -exec rm -rf {} \;
@@ -16,5 +16,5 @@ find /opt/ockb/bin/backup/ -name '*.json' -type f -mtime +7 -exec rm -rf {} \;
 # 0 1 * * * /opt/ockb/bin/backup/backup.sh
 #
 # Also can use crontab to send the backup file to backup server
-# 10 5 * * 0 find ./ -name '*.json' -type f -mtime -1 -exec scp {} root@<backup server>:/<backup folder> \;
+# 10 5 * * 0 find /opt/ockb/bin/backup/ -name '*.json' -type f -mtime -1 -exec scp {} root@<backup server>:/<backup folder> \;
 ############################################################
